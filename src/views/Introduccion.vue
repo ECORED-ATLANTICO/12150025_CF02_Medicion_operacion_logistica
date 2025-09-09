24634fd063d82921c5ba11f2a91710dc05a80a38
devcontenidosinteractivos <98177573+AndresDiazPinto@users.noreply.github.com>
2025-09-09 16:22:52 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,8 +28,8 @@
 
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/8Mqife5iF3w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/uR-J2ClbbrQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      
 </template>
 
 <script>
